915bb0e2bdda2809bda25d41a6512def971de006
frizzayyazzirf <murilolavez04@gmail.com>
2026-01-22 06:39:56 -0300

    Update Tabela Hash Avançada.c

--- Tabela Hash Avançada.c
+++ Tabela Hash Avançada.c
@@ -2,123 +2,133 @@
 #include <stdlib.h>
 #include <string.h>
 #include "funsaoes.h"
-
 typedef struct {
-    NoLista *lista;
-} TabelaHash;
+    No *tab;
+}hash;
 
-void inicializar(TabelaHash tabela[], int tamanhoHash){
-    for(int i = 0; i < tamanhoHash; i++){
-        tabela[i].lista = NULL;
+void ini(hash tabe[], int tamhash){
+    for(int i = 0; i < tamhash; i++){
+        tabe[i].tab = NULL;
+        
     }
 }
 
-void imprimirLista(NoLista *lista){
-    NoLista *aux = lista;
+void imprimirlista(No *l){
+    No *aux;
+    aux = l;
     while(aux){
         printf("%d ", aux->prio);
         aux = aux->prox;
     }
 }
 
-void imprimirTabela(TabelaHash *tabela, int tamanho){
-    for(int i = 0; i < tamanho; i++){
+void imprimirtab(hash *t, int tam){
+    for(int i = 0; i < tam; i++){
         printf("indice %d: ", i);
-        imprimirLista(tabela[i].lista);
+        imprimirlista(t[i].tab);
         printf("\n");
     }
 }
 
-int criarTabela(int tamanho){
-    int candidato, naoPrimo = 1;
-    candidato = tamanho * 2;
-
-    while(naoPrimo){
-        naoPrimo = 0;
-        for(int i = 2; i < candidato; i++){
-            if(candidato % i == 0){
-                candidato++;
-                naoPrimo = 1;
-                break;
+int criatab(int tam){
+int b, aux = 1;
+    b = tam * 2;
+    //verificação de numero primo
+    while(aux >= 1){
+        //Definição para que o loop continue caso b seja divisivel por outro numero alem de 1 e ele mesmo
+        aux = 0;
+        //primeiro loop, verifica todos os numeros possiveis capazes de dividir
+        for(int i = 1; i<=b; i++ ){
+            if(i != 1 && i != b){
+                if (b % i == 0){
+                    printf("Nao e primo.");
+                    b++;
+                    //detecção de numero nao primo, aux++, logo, aux != 0, codigo rodara novamente
+                    aux++;
+                    break;
+                }
             }
         }
     }
-    printf(" %d\n", candidato);
-    return candidato;
+    printf(" %d\n", b);
+    return b;
 }
 
-int funcaoHash(int valor, int tamanho){
-    return valor % tamanho;
+int funha(int val, int tam){
+    int ind;
+    ind = val % tam;
+    return ind;
 }
 
-void inserirLista(NoLista **topo, int valor){
-    NoLista *novo = malloc(sizeof(NoLista));
-    novo->prio = valor;
-    novo->prox = *topo;
-    *topo = novo;
+void addinte(No **topo, int valor) {
+	No *novo = malloc(sizeof(No));
+	novo->prio = valor;
+	novo->prox = *topo;
+	*topo = novo;
 }
 
-void inserirValor(TabelaHash tabela[], int valor, int tamanho){
-    int indice = funcaoHash(valor, tamanho);
-    inserirLista(&tabela[indice].lista, valor);
+void inserir(hash tabe[], int val, int tam){
+    int ind;
+    ind = funha(val, tam);
+    addinte(&tabe[ind].tab, val);
     printf("Valor inserido!\n");
 }
 
-int buscarValor(TabelaHash tabela[], int valor, int tamanho){
-    int indice = funcaoHash(valor, tamanho);
-    NoLista *aux = tabela[indice].lista;
-
-    while(aux){
-        if(aux->prio == valor){
+int busca(hash tabe[], int val, int tam){
+    int ind;
+    No *aux;
+    ind = funha(val , tam);
+    aux = tabe[ind].tab;
+    while(aux != NULL && aux->prio != val){
+        if(aux->prio == val){
             printf("Valor encontrado\n");
             return aux->prio;
+        } else{
+            aux = aux->prox;
         }
-        aux = aux->prox;
     }
-
+    if(aux){
+        if(aux->prio == val){
+            printf("Valor encontrado");
+            return aux->prio;
+        }
+    }
     printf("Valor nao encontrado\n");
     return 0;
 }
 
 int main() {
-    int tamanhoHash, tamanho = 0, valorBusca, opcao = 1, valorInserir;
-
-    while(!tamanho){
+    int tamhash, tam = 0, buscar, esc, ins;
+    while(!tam){
         printf("Qual o tamanho da tabela?\n");
-        scanf("%d", &tamanho);
+        scanf("%d", &tam);
     }
-
-    tamanhoHash = criarTabela(tamanho);
-
-    TabelaHash *tabela = malloc(sizeof(TabelaHash) * tamanhoHash);
-    inicializar(tabela, tamanhoHash);
-
-    while(opcao){
+    tamhash = criatab(tam);
+    hash *t;
+    t = malloc(sizeof(hash) * tamhash);
+    ini(t, tamhash);
+    while(esc){
         printf("1 Para imprimir, 2 para inserir, 3 para buscar e 0 para fechar\n");
-        scanf("%d", &opcao);
-
-        switch(opcao){
-            case 2:
-                scanf("%d", &valorInserir);
-                inserirValor(tabela, valorInserir, tamanhoHash);
-                break;
-
-            case 1:
-                imprimirTabela(tabela, tamanhoHash);
-                break;
-
-            case 3:
-                scanf("%d", &valorBusca);
-                valorBusca = buscarValor(tabela, valorBusca, tamanhoHash);
-                if(valorBusca)
-                    printf(": %d\n", valorBusca);
-                break;
-
-            case 0:
-                break;
-
-            default:
-                printf("Escolha invalida!\n");
+        scanf("%d", &esc);
+        switch(esc){
+            case 2 :
+            ins = 0;
+            scanf("%d", &ins);
+            inserir(t , ins, tamhash);
+            break;
+            case 1 : 
+            imprimirtab(t, tamhash);
+            break;
+            case 3 : 
+            scanf("%d", &buscar);
+            buscar = busca(t, buscar, tamhash);
+            if(buscar)
+                printf(": %d\n", buscar);
+            break;
+            case 0 :
+            break;
+            default :
+            printf("Escolha invalida!\n");
         }
     }
 }
