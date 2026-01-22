80cef06484b3f2970b5b1e900f57941accb633a7
frizzayyazzirf <murilolavez04@gmail.com>
2026-01-22 06:42:59 -0300

    Update Tabela Hash Simples.c

--- Tabela Hash Simples.c
+++ Tabela Hash Simples.c
@@ -3,93 +3,139 @@
 #include <string.h>
 #include "funsaoes.h"
 
-typedef struct {
-    NoLista *lista;
-} TabelaHash;
-
-void inicializar(TabelaHash tabela[], int tamanhoHash){
-    for(int i = 0; i < tamanhoHash; i++){
-        tabela[i].lista = NULL;
-    }
-}
-
-int criarTabela(int tamanho){
-    int candidato, composto = 1;
-    candidato = tamanho * 2;
-
-    // verificação de número primo
-    while(composto){
-        composto = 0;
-        for(int i = 2; i < candidato; i++){
-            if(candidato % i == 0){
-                printf("Nao e primo.");
-                candidato++;
-                composto = 1;
-                break;
+int criatab(int tam){
+int b, aux = 1;
+    b = tam * 2;
+    //verificação de numero primo
+    while(aux >= 1){
+        //Definição para que o loop continue caso b seja divisivel por outro numero alem de 1 e ele mesmo
+        aux = 0;
+        //primeiro loop, verifica todos os numeros possiveis capazes de dividir
+        for(int i = 1; i<=b; i++ ){
+            if(i != 1 && i != b){
+                if (b % i == 0){
+                    printf("Nao e primo.");
+                    b++;
+                    //detecção de numero nao primo, aux++, logo, aux != 0, codigo rodara novamente
+                    aux++;
+                    break;
+                }
             }
         }
     }
-    printf(" %d\n", candidato);
-    return candidato;
+    printf(" %d\n", b);
+    return b;
 }
 
-int funcaoHash(int valor, int tamanho){
-    return valor % tamanho;
+void fatorcarga(int tam, int tamhash){
+    float aux;
+    //calculo fator de carga
+    aux = (float)tam / tamhash;
+    //avisos
+    if(aux < 0.30)
+    printf("Desperdicio de memoria\n");
+    else if (aux > 0.70)
+    printf("Pouco espaco vago\n");
 }
-
-void inserirLista(NoLista **topo, int valor) {
-    NoLista *novo = malloc(sizeof(NoLista));
-    novo->prio = valor;
-    novo->prox = *topo;
-    *topo = novo;
+int* funchash(int conjun[], int tamhash, int tam){
+    int tab[tamhash][2], *a, aux = 0;
+    a = malloc(criatab(tam) * sizeof(int));
+    //espalhamento de indices
+    for(int i = 0; i < tam; i++){
+        tab[i][0] = conjun[i] % tamhash;
+        tab[i][1] = conjun[i];
+        printf("%d\n", tab[i][0]);
+    }
+    for(int i = 0; i<tamhash; i++)
+    a[i] = 0;
+//passar todos valroes registrados a tabela
+        for(int j = 0; j<tam; j++){
+            if(!a[tab[j][0]]){
+                 a[tab[j][0]] = tab[j][1];
+            } else {
+               for(int i = 0; i <tamhash; i++){
+                    if(!a[i]){
+                 a[i] = tab[j][1];
+                 break;
+            }
+               }
+            }
+    }
+    printf("HASH\n");
+      for(int i = 0; i<tamhash; i++)
+      printf("%d\n", a[i]);
+    return a;
 }
-
-void inserirTabela(TabelaHash tabela[], int valor, int tamanho){
-    int indice = funcaoHash(valor, tamanho);
-    inserirLista(&tabela[indice].lista, valor);
+void addhash(int **hash, int val, int tam){
+    int aux, i = 0;
+    aux = val % tam;
+    printf("%d", aux);
+    if(!hash[0][aux])
+    hash[0][aux] = val;
+    else {
+        if(!hash[0][i]){
+            hash[0][i] = val;
+        }
+        else
+        while(hash[0][i]){
+            if(!hash[0][i]){
+                hash[0][i] = val;
+                break;
+            }
+            i++;
+        }
+    }
+    printf("\n\n\n\n");
+    for(int i = 0; i<tam; i++){
+        printf("%d\n", hash[0][i]);
+    }
+    printf("\n\n\n\n");
 }
-
-int buscarTabela(TabelaHash tabela[], int valor, int tamanho){
-    int indice = funcaoHash(valor, tamanho);
-    NoLista *aux = tabela[indice].lista;
-
-    while(aux && aux->prio != valor){
-        aux = aux->prox;
+int* buscarHash(int hash[], int ind, int tam){
+    int aux, b = 0;
+    aux = ind % tam;
+    if (hash[aux]!=ind){
+        for(int i = 0; i < tam; i++){
+            printf("valores percorridos: %d\n", hash[i]);
+            if (hash[i]==ind){
+                printf("valor encontrado mas na posicao incorreta %d\n", hash[i]);
+                b++;
+                return &hash[i];
+            }
+            if(b)
+                break;
+        }
+    } else if (hash[aux]==ind){
+        printf("valor encontrado %d\n", hash[aux]);
+        return &hash[aux];
     }
 
-    if(aux && aux->prio == valor){
-        printf("Valor encontrado\n");
-        return aux->prio;
-    }
-
-    printf("Valor nao encontrado\n");
-    return 0;
+} 
+void retirar(int hash[], int ind, int tam){
+    int *pont;
+    pont = buscarHash(hash, ind, tam);
+    printf("teste %d\n", *pont);
+    *pont = 0;
+    printf("teste %d", *pont);
 }
 
 int main() {
-    int tamanhoHash, tamanho = 6, valorBusca;
-
-    printf("Qual o tamanho da tabela?\n");
-    scanf("%d", &tamanho);
-
-    tamanhoHash = criarTabela(tamanho);
-
-    TabelaHash *tabela = malloc(sizeof(TabelaHash) * tamanhoHash);
-    inicializar(tabela, tamanhoHash);
-
-    inserirTabela(tabela, 27, tamanhoHash);
-    inserirTabela(tabela, 5, tamanhoHash);
-    inserirTabela(tabela, 87, tamanhoHash);
-    inserirTabela(tabela, 9, tamanhoHash);
-
-    for(int i = 0; i < tamanhoHash; i++){
-        if(tabela[i].lista)
-            printf("%d\n", tabela[i].lista->prio);
+    int conjunt[100], *a = NULL, tam = 8, tamhash, p = 1;
+    scanf("%d", &tam);
+    for(int i = 0; i < tam; i++){
+        scanf("%d", &conjunt[i]);
     }
-
-    int entrada;
-    scanf("%d", &entrada);
-
-    valorBusca = buscarTabela(tabela, entrada, tamanhoHash);
-    printf("VALOR AQ O %d", valorBusca);
+    a = malloc(criatab(tam) * sizeof(int));
+    tamhash = criatab(tam);
+    fatorcarga(tam, tamhash);
+   a = funchash(conjunt, tamhash, tam);
+   addhash(&a, 2,tamhash);
+   retirar(a, 2, tamhash);
+   while(p == 1){
+        int x;
+        scanf("%d", &x);
+        buscarHash(a, x, tamhash);
+   } 
+   
+   
 }
